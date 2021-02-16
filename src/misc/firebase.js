0f7cf1d2e91bc9e8381c76d0b990353e9e2cf966
Nazarii <nazariishparhalo@gmail.com>
2021-02-16 15:31:07 +0200

    initiazlized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+    apiKey: "AIzaSyDz0Cpx77gfsGLfzX-VCvDO7zdEKI--z4I",
+    authDomain: "chat-web-app-f5647.firebaseapp.com",
+    projectId: "chat-web-app-f5647",
+    storageBucket: "chat-web-app-f5647.appspot.com",
+    messagingSenderId: "227495898005",
+    appId: "1:227495898005:web:fb95b0e54aad4e364b649c"
+};
+
+const app = firebase.initializeApp(config);
