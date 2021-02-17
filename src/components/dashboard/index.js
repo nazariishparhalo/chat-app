d377b5b756b8fb23a1055a32c9497e9e99f36724
Nazarii <nazariishparhalo@gmail.com>
2021-02-18 00:48:14 +0200

    created reuseble editable input

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,31 +1,39 @@
-import React, { Profiler } from 'react'
-import { Button, Drawer } from 'rsuite'
+import React from 'react'
+import { Button, Divider, Drawer } from 'rsuite'
+import EditableInput from '../EditableInput'
 import { useProfile } from '../../context/profile.context'
 
 const Dashboard = ({onSignOut}) => {
 
     const {profile} = useProfile();
 
+    const onSave = async (newData) => {
+        console.log('newData', newData)
+    };
+
     return (
         <>
-        <Drawer.Header>
-            <Drawer.Title>
-                Dashboard
-            </Drawer.Title>
-        </Drawer.Header>
+            <Drawer.Header>
+                <Drawer.Title>
+                    Dashboard
+                </Drawer.Title>
+            </Drawer.Header>
 
-        <Drawer.Body>
-            <h3>Hey, {profile.name}</h3>
-        </Drawer.Body>
+            <Drawer.Body>
+                <h3>Hey, {profile.name}</h3>
+                <Divider />
 
-        <Drawer.Footer>
-            <Button block color="red" onClick={onSignOut}>
-                Sign out
-            </Button>
+                <EditableInput name="nickname" initialValue={profile.name} onSave={onSave} label={<h6 className="mb-2">Nickname</h6>} />
+            </Drawer.Body>
 
-        </Drawer.Footer>
-
+            <Drawer.Footer>
+                <Button block color="red" onClick={onSignOut}>
+                    Sign out
+                </Button>
+            </Drawer.Footer>
         </>
+
+
     )
 }
 
