3fe30a04d6c211bbfb360b85299a1f525ad1ca9b
Nazarii <nazariishparhalo@gmail.com>
2021-02-19 18:16:06 +0200

    added buttom to grant or revoke admin access

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { memo } from 'react'
+import { Button } from 'rsuite';
 import TimeAgo from 'timeago-react'
+import { useCurrentRoom } from '../../../context/current-room.context';
+import { auth } from '../../../misc/firebase';
 import PresenceDot from '../../PresenceDot';
 import ProfileAvatar from '../../ProfileAvatar' 
 import ProfileInfoBtnModal from './ProfileInfoBtnModal';
 
-const MessageItem = ({message}) => {
+const MessageItem = ({message, handleAdmin }) => {
 
     const {author, createdAt, text} = message;
+
+    const isAdmin = useCurrentRoom(v => {return v.isAdmin});
+    const admins = useCurrentRoom(v => {return v.admins});
+
+    const isMsgAuthorAdmin = admins.includes(author.uid);
+    const isAuthor = auth.currentUser.uid === author.uid;
+    const canGrantAdmin = isAdmin && !isAuthor;
+
     return (
         <li className="padded mb-1">
             <div className="d-flex align-items-center fond-bolder mb-1">
                 <PresenceDot uid={author.uid} />
                 <ProfileAvatar src={author.avatar} name={author.name} className="ml-1" size="xs" />
-                <ProfileInfoBtnModal profile={author} appearance="link" className="p-0 ml-1 text-black" />
+                <ProfileInfoBtnModal profile={author} appearance="link" className="p-0 ml-1 text-black">
+                    {canGrantAdmin && <Button block onClick={() => {handleAdmin(author.uid)}} color="blue">{isMsgAuthorAdmin ? 'Remove admin permission' : 'Give admin permission'}</Button>}
+                    
+                </ProfileInfoBtnModal>
                 <TimeAgo datetime={createdAt} className="font-normal text-black-45 ml-2"/>
             </div>
             <div>
@@ -22,4 +36,4 @@ const MessageItem = ({message}) => {
     )
 }
 
-export default MessageItem
+export default memo(MessageItem)
