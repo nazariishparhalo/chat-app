350074a5754c2bdc48c5092b136bcd30c04c5835
Nazarii <nazariishparhalo@gmail.com>
2021-02-19 13:23:14 +0200

    update all user references if data is updated

--- src/components/dashboard/AvatarUploadBtn.js
+++ src/components/dashboard/AvatarUploadBtn.js
@@ -5,6 +5,7 @@ import { useModalState } from '../../misc/custom-hooks'
 import { database, storage } from '../../misc/firebase';
 import { useProfile } from '../../context/profile.context';
 import ProfileAvatar from '../ProfileAvatar';
+import { getUserUpdates } from '../../misc/helpers';
 
 const fileInputTypes = ".png, .jpeg, .jpg";
 
@@ -65,9 +66,9 @@ const AvatarUploadBtn = () => {
 
             const downloadUrl = await uploadAvatarResult.ref.getDownloadURL();
 
-            const userAvatarRef = database.ref(`profiles/${profile.uid}`).child('avatar');
+            const updates  = await getUserUpdates(profile.uid, 'avatar', downloadUrl, database);
+            await database.ref().update(updates);
 
-            userAvatarRef.set(downloadUrl);
 
             setIsLoading(false);
             Alert.info('Avatar has been uploaded', 4000);
@@ -81,7 +82,7 @@ const AvatarUploadBtn = () => {
     return (
         <div className="mt-3 text-center">
 
-            <ProfileAvatar src={profile.avatar} name={profile.name} className="width-200 height-200 img-fullsize font-huge" />
+            <ProfileAvatar src={profile.avatar} name={profile.name} className="width-200 height-200 img-fullsize font-huge " />
 
             <div>
                 <label htmlFor="avatar-upload" className="d-block cursor-pointer padded">
