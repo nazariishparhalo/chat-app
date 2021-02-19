58d1258282ab79bb0c4257346d19e18ae02af62a
Nazarii <nazariishparhalo@gmail.com>
2021-02-19 12:34:14 +0200

    created chat bottom; send messages to the chat

--- src/components/chat-window/bottom/index.js
+++ src/components/chat-window/bottom/index.js
@@ -1,9 +1,82 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
+import { useParams } from 'react-router'
+import { Alert, Icon, Input, InputGroup } from 'rsuite'
+import firebase from 'firebase/app'
+import { useProfile } from '../../../context/profile.context'
+import { database } from '../../../misc/firebase'
+
+function assembelMessage(profile, chatId) {
+    return {
+        roomId: chatId,
+        author: {
+            name: profile.name,
+            uid: profile.uid,
+            createdAt: profile.createdAt,
+            ...(profile.avatar ? { avatar: profile.avatar} : {})
+        },
+        createdAt: firebase.database.ServerValue.TIMESTAMP,
+
+
+    }
+}
 
 const ChatBottom = () => {
+
+    const [input, setInput] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
+    const {chatId} = useParams();
+    const {profile} = useProfile();
+
+    const onInputChange = useCallback((value) => {
+        setInput(value);
+    }, []);
+
+    const onSnedClick = async () => {
+        if(input.trim() === '') {
+            return;
+        }
+
+        const msgData = assembelMessage(profile, chatId);
+        msgData.text = input;
+
+        const updates = {};
+
+        const messageId = database.ref('messages').push().key;
+
+        updates[`/messages/${messageId}`] = msgData;
+        updates[`/rooms/${chatId}/lastMessage`] = {
+            ...msgData,
+            msgId: messageId
+        }
+
+        setIsLoading(true);
+        try {
+            await database.ref().update(updates);
+            setInput('');
+            setIsLoading(false);
+        } catch (err) {
+            setIsLoading(false);
+            Alert.error(err.message, 4000);
+        }
+    };
+
+    const onKeyDown = (ev) => {
+        if(ev.keyCode === 13) {
+            ev.preventDefault();
+            onSnedClick();
+        }
+    };
+
+
     return (
         <div>
-            bottom
+            <InputGroup>
+                <Input placeholder ="Write a new message here..." value={input} onChange={onInputChange} onKeyDown={onKeyDown} />
+                <InputGroup.Button color="blue" appearance="primary" onClick={onSnedClick} disabled={isLoading}>
+                    <Icon icon="send" />
+                </InputGroup.Button>
+            </InputGroup>
         </div>
     )
 }
